6882344cf7fb0a0c274d1325d1ba8a563fa2bd5b
Karl Behrens <karlbehrensg@gmail.com>
2022-06-29 23:58:01 -0400

    Base configuration for db

---
+++ config/dbConfig.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const dbCondig = () => {
+  if (process.env.NODE_ENV === "test") {
+    return "sqlite::memory:";
+  }
+
+  return {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    schema: process.env.DB_SCHEMA,
+  };
+};
+
+module.exports = dbCondig();
