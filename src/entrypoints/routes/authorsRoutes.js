5b313dafa77b898d2bf400547b65c3d82a8eef53
Karl Behrens <karlbehrensg@gmail.com>
2022-06-30 16:53:18 -0400

    Retrive authors from db

--- src/entrypoints/routes/authorsRoutes.js
+++ src/entrypoints/routes/authorsRoutes.js
@@ -6,16 +6,33 @@ const { Author } = require("src/domain/models");
 router.get("", async (req, res) => {
   // Check if a query value come into url: baseurl/authors?custom_query=my_custom_query_value
   if (req.query.custom_query) {
-    res.send(`Get authors with custom_query: ${req.query.custom_query}`);
-  } else {
-    const authors = await Author.findAll();
-    res.send(authors);
+    const custom_query = req.query.custom_query;
+    const author = await Author.findByPk(custom_query);
+
+    if (author === null) {
+      return res
+        .status(404)
+        .send({ message: `Author with id ${custom_query} not found` });
+    }
+
+    return res.send(author);
   }
+  const authors = await Author.findAll();
+  res.send(authors);
 });
 
-router.get("/:my_param", (req, res) => {
+router.get("/:my_param", async (req, res) => {
   // Get my_param value in url: baseurl/authors/my_custom_param_value
-  res.send(`Get author with url param: ${req.params.my_param}`);
+  const my_param = req.params.my_param;
+  const author = await Author.findByPk(my_param);
+
+  if (author === null) {
+    return res
+      .status(404)
+      .send({ message: `Author with id ${my_param} not found` });
+  }
+
+  return res.send(author);
 });
 
 router.post("", (req, res) => {
