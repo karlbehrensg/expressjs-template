98c96355c05aa0ed9c4a82caf21f12201ca090ff
Karl Behrens <karlbehrensg@gmail.com>
2022-07-05 10:57:30 -0400

    Add error controler to retrive book

--- src/controllers/books/retriveBook.controller.js
+++ src/controllers/books/retriveBook.controller.js
@@ -1,5 +1,16 @@
-const retriveBook = async (req, res) => {
-  res.send(`Obtener libro con paramtro: ${req.params.id}`);
+const { ApiError } = require("src/errors");
+
+const retriveBook = async (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    next(
+      ApiError.badRequest(
+        `El id debe ser numerico, pero se envio ${req.params.id}`
+      )
+    );
+    return;
+  }
+  res.send(`Obtener libro con paramtro: ${id}`);
 };
 
 module.exports = retriveBook;
