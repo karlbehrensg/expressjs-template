41fbd17fa905206c816ea07858bfe26c236927eb
Karl Behrens <karlbehrensg@gmail.com>
2022-07-05 15:18:41 -0400

    Retrive book by id

--- src/controllers/books/deleteBook.controller.js
+++ src/controllers/books/deleteBook.controller.js
@@ -1,5 +1,24 @@
+const { ApiError } = require("src/errors");
+const { Book } = require("src/models");
+
 const deleteBook = async (req, res) => {
-  res.send(`Libro con id ${req.params.id} fue eliminado`);
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    next(ApiError.badRequest("id debe ser numerico"));
+    return;
+  }
+
+  const book = await Book.findByPk(id);
+
+  if (!book) {
+    next(ApiError.notFound(`No se encontro el libro con el id: ${id}`));
+    return;
+  }
+
+  book.destroy();
+
+  res.send(`Libro con id ${id} fue eliminado`);
 };
 
 module.exports = deleteBook;
--- src/controllers/books/retriveBook.controller.js
+++ src/controllers/books/retriveBook.controller.js
@@ -1,16 +1,22 @@
 const { ApiError } = require("src/errors");
+const { Book } = require("src/models");
 
 const retriveBook = async (req, res, next) => {
   const id = parseInt(req.params.id);
+
   if (isNaN(id)) {
-    next(
-      ApiError.badRequest(
-        `El id debe ser numerico, pero se envio ${req.params.id}`
-      )
-    );
+    next(ApiError.badRequest("id debe ser numerico"));
     return;
   }
-  res.send(`Obtener libro con paramtro: ${id}`);
+
+  const book = await Book.findByPk(id);
+
+  if (!book) {
+    next(ApiError.notFound(`No se encontro el libro con el id: ${id}`));
+    return;
+  }
+
+  res.send(book);
 };
 
 module.exports = retriveBook;
