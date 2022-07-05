639e198fba08356f8bce64ba3b7c3fe97c6d0f86
Karl Behrens <karlbehrensg@gmail.com>
2022-07-05 18:52:50 -0400

    Test retrive books

--- test/e2e/books.test.js
+++ test/e2e/books.test.js
@@ -1,3 +1,17 @@
-test("Retrive all books", () => {
-  expect([]).toEqual([]);
+const request = require("supertest");
+const app = require("src/app");
+const { conn } = require("src/adapters");
+
+describe("Test Books URLs", () => {
+  beforeAll(async () => {
+    await conn.sync({ force: true });
+  });
+
+  it("should fail when accessing an authed route with an invalid JWT", async () => {
+    await request(app).get("/api/books").expect(200).expect([]);
+  });
+
+  afterAll(async () => {
+    await conn.close();
+  });
 });
