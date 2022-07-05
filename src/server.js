3afa5a63c6250b6d3e4de376382ca4c74de54d51
Karl Behrens <karlbehrensg@gmail.com>
2022-07-05 16:22:27 -0400

    Sepatare app from server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,5 @@
-const express = require("express");
+const app = require("src/app");
 const PORT = process.env.PORT || 3000;
-
-const { apiErrorHandler } = require("src/errors");
-const router = require("src/routes");
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
-app.use(apiErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening in port ${PORT}`);
