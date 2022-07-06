984aa5a000755c091b688488ba484258cf36731f
Karl Behrens <karlbehrensg@gmail.com>
2022-07-05 20:23:00 -0500

    Apply global error handler (#2)

--- src/controllers/books/updateBook.controller.js
+++ src/controllers/books/updateBook.controller.js
@@ -2,28 +2,32 @@ const { ApiError } = require("src/errors");
 const { Book } = require("src/models");
 
 const updateBook = async (req, res, next) => {
-  const id = parseInt(req.params.id);
+  try {
+    const id = parseInt(req.params.id);
 
-  if (isNaN(id)) {
-    next(ApiError.badRequest("id debe ser numerico"));
-    return;
+    if (isNaN(id)) {
+      next(ApiError.badRequest("id debe ser numerico"));
+      return;
+    }
+
+    const book = await Book.findByPk(id);
+
+    if (!book) {
+      next(ApiError.notFound(`No se encontro el libro con el id: ${id}`));
+      return;
+    }
+
+    const { title } = req.body;
+
+    book.set({
+      title,
+    });
+    book.save();
+
+    res.status(200).send(book);
+  } catch (err) {
+    next(err);
   }
-
-  const book = await Book.findByPk(id);
-
-  if (!book) {
-    next(ApiError.notFound(`No se encontro el libro con el id: ${id}`));
-    return;
-  }
-
-  const { title } = req.body;
-
-  book.set({
-    title,
-  });
-  book.save();
-
-  res.status(200).send(book);
 };
 
 module.exports = updateBook;
